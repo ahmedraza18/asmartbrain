4aeff377ee64e2d5ba1038966962e688fa824a81
Ahmed Raza <artistahmed15@gmail.com>
2021-02-18 00:35:17 +0530

    Update image.js

--- smartbrain-api/controllers/image.js
+++ smartbrain-api/controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: "714cdd4f7693470fb88cc1c904dd58fa",
+    apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
